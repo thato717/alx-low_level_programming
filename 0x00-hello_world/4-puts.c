f99eee7f28fa27af101b149fb70ea6fd23c1b00c
Thato <thatootaku7@gmail.com>
2023-04-24 13:30:27 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/*
+ * Betty documentation
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle\n");
+        return(0);
 }
