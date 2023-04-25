ade6c17e80330fda46824f312483c28a5902e273
Thato <thatootaku7@gmail.com>
2023-04-25 16:40:48 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry poitn
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("_putchar\n");
+	return (0);
 }
