c1d697689a4d08de67220cceefa52cb3f46ff0db
Thato <thatootaku7@gmail.com>
2023-05-03 16:23:32 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * int_strein - returns length of string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longp = 0;
+
+	while (*s != '\0')
+	{
+		longp++;
+		s++;
+	}
+
+	return (longp);
+}
