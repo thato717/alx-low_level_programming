bfab2f1b37bd387d7decba1735024e2304dff3c8
Thato <thatootaku7@gmail.com>
2023-05-04 16:25:08 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - changes lowercase to uppercase
+ * @n: input
+ * Return: uppercase string
+ */
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
