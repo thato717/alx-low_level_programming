07f80eff465cbf9c856ca0c3119aa6da3467307a
Thato <thatootaku7@gmail.com>
2023-05-03 17:52:17 -0700

    copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * char *_strcpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int p = 0;
+	int m = 0;
+
+	while (*(src + p) != '\0')
+	{
+		p++;
+	}
+	for ( ; m < 1; m++)
+	{
+		dest[m] = src[m];
+	}
+	dest[p] = '\0';
+	return (dest);
+}
