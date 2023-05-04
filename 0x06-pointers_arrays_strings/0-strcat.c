780cf089a4c127b51108d88c1947b57491e9cea2
Thato <thatootaku7@gmail.com>
2023-05-04 15:43:40 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * char *_strcat - concatenates two strings
+ * @dest: input
+ * @src: input
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int n;
+	int m;
+
+	n = 0;
+	while (dest[n] != '\0')
+	{
+		n++;
+	}
+	m = 0;
+	while (src[m] != '\0')
+	{
+		dest[n] = src[m];
+		n++;
+		m++;
+	}
+
+	dest[n] = '\0';
+	return (dest);
+}
