db3720d3cf4251f8e2e68fc1f637a6e42b547772
Thato <thatootaku7@gmail.com>
2023-05-08 17:53:01 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - gets length of a prefix substring
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x = 0;
+	int y;
+
+	while (*s)
+	{
+		for (y = 0; accept[y]; y++)
+		{
+			if (*s == accept[y])
+			{
+				x++;
+				break;
+			}
+			else if (accept[y + 1] == '\0')
+				return (x);
+		}
+		s++;
+	}
+	return (x);
+}
