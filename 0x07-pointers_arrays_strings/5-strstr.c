88b4124a99fe8974894b4f12725dd0d9a18941fd
Thato <thatootaku7@gmail.com>
2023-05-08 18:08:27 -0700

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *x = haystack;
+		char *y = needle;
+
+		while (*x == *y && *y != '\0')
+		{
+			x++;
+			y++;
+		}
+
+		if (*y == '\0')
+			return (haystack);
+	}
+	return (0);
+}
